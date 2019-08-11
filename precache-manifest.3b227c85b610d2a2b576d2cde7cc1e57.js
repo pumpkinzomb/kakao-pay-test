d08c1afe4f4bbc5a1b7b464563e3b2eed8baad82
pumpkinzomb <pumpkinzomb@gmail.com>
2019-08-11 13:48:58 +0900

    Updates

--- precache-manifest.3b227c85b610d2a2b576d2cde7cc1e57.js
+++ precache-manifest.3b227c85b610d2a2b576d2cde7cc1e57.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3f9e771fc67d6983a0272176c514e7d1",
+    "revision": "b0307e05cbe2ca8853c4645d99da0335",
     "url": "/kakao-pay-test/index.html"
   },
   {
-    "revision": "46e13d3a91951e7bd9d6",
+    "revision": "9fdd975710c5dd839dfe",
     "url": "/kakao-pay-test/static/css/main.ae9fc3ca.chunk.css"
   },
   {
-    "revision": "873cccb493ab58d78e43",
-    "url": "/kakao-pay-test/static/js/2.58a14bc3.chunk.js"
+    "revision": "66cc7a69ccf24a1e31cc",
+    "url": "/kakao-pay-test/static/js/2.65e70970.chunk.js"
   },
   {
-    "revision": "46e13d3a91951e7bd9d6",
-    "url": "/kakao-pay-test/static/js/main.f6f82d38.chunk.js"
+    "revision": "9fdd975710c5dd839dfe",
+    "url": "/kakao-pay-test/static/js/main.dd947e17.chunk.js"
   },
   {
     "revision": "8a257261f46c1ae130c4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kakao-pay-test/precache-manifest.4a1ec1e733b234a08387978a0a93feea.js"
+  "/kakao-pay-test/precache-manifest.3b227c85b610d2a2b576d2cde7cc1e57.js"
 );
 
 self.addEventListener('message', (event) => {
