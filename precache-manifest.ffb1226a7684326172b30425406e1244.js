ffe2e8197c1c9f57d849225918f1b7c154556f28
pumpkinzomb <pumpkinzomb@gmail.com>
2019-08-11 13:13:15 +0900

    Updates

---
+++ precache-manifest.ffb1226a7684326172b30425406e1244.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b945c274eb60cffdb442bf85cc2a2a",
+    "url": "/kakao-pay-test/index.html"
+  },
+  {
+    "revision": "5e16730557c95e29339f",
+    "url": "/kakao-pay-test/static/css/main.ae9fc3ca.chunk.css"
+  },
+  {
+    "revision": "873cccb493ab58d78e43",
+    "url": "/kakao-pay-test/static/js/2.58a14bc3.chunk.js"
+  },
+  {
+    "revision": "5e16730557c95e29339f",
+    "url": "/kakao-pay-test/static/js/main.de021447.chunk.js"
+  },
+  {
+    "revision": "8a257261f46c1ae130c4",
+    "url": "/kakao-pay-test/static/js/runtime~main.e62858f1.js"
+  }
+]);
